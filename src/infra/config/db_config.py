00734ab9c34a3825312c9cf2ee8be31de97b580b
matheus <mcerozi@gmail.com>
2022-06-28 12:33:46 -0300

    feat: implementing repo

--- src/infra/config/db_config.py
+++ src/infra/config/db_config.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 class DBConnectionHandler:
     """ SQLAlchemy DB connection"""
-
 
     def __init__(self) -> None:
         self.__connection_string = 'sqlite:///storage.db'
@@ -12,3 +12,12 @@ class DBConnectionHandler:
         """ Return engine connection """
         engine = create_engine(self.__connection_string)
         return engine
+
+    def __enter__(self):
+        engine = self.get_engine()
+        session_local = sessionmaker(engine)
+        self.session = session_local()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
---
+++ src/infra/repo/__init__.py
@@ -0,0 +1,1 @@
+from .fake_repo import FakerRepo
